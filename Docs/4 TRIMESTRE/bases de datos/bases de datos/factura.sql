89dd0b526ddcb54675491ac9e3752784d619a32b
LauraVDB567 <162857157+LauraVDB567@users.noreply.github.com>
2025-02-25 13:42:36 -0500

    Delete Docs/4 TRIMESTRE directory

--- Docs/4 TRIMESTRE/bases de datos/bases de datos/factura.sql
+++
@@ -1,9 +0,0 @@
-create table factura(
-nombre varchar (40) primary key,
-correo varchar (250),
-telefono varchar (40),
-numeroFactura varchar (40),
-productos varchar (200),
-total double,
-metodoPago varchar (40)
-);
--- Docs/4 TRIMESTRE/bases de datos/bases de datos/usuarios.sql
+++
@@ -1,7 +0,0 @@
-create table usuarios (
-id int not null auto_increment primary key,
-apodo varchar (40),
-apellido varchar (40),
-correo varchar (40),
-contraseña varchar (255)
-);
