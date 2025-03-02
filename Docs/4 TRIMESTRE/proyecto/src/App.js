7d9a8562f8cdb135b6b28ff09569758dc83d6599
LuisaCastillo <163435574+LuisaCastillo109@users.noreply.github.com>
2025-03-02 15:10:58 -0500

    Update App.js

--- Docs/4 TRIMESTRE/proyecto/src/App.js
+++ Docs/4 TRIMESTRE/proyecto/src/App.js
@@ -2,17 +2,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Principal from './InicioYRegistro/Principal.jsx';
 import InicioYRegistro from './InicioYRegistro/InicioYRegistro.jsx';
 import Terminosycondiciones from './InicioYRegistro/Terminos y condiciones.jsx';
-import Recover from './InicioYRegistro/RecoverPassword.jsx';
-import Reset from './InicioYRegistro/ResetPassword.jsx';
 import Carrito from './InicioYRegistro/carrito.jsx';
 import Crud from "./InicioYRegistro/crud.jsx";
 import Devolucion from "./InicioYRegistro/devolucion.jsx";
 import Factura from './InicioYRegistro/factura.jsx'; 
 import VerMas from './InicioYRegistro/vermas.jsx';
+import Recover from './InicioYRegistro/RecoverPassword.jsx';
+import Reset from './InicioYRegistro/ResetPassword.jsx';
 import Solicitud from './InicioYRegistro/solicitud.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import Privacidad from './InicioYRegistro/privacidad.js';
+import ModificarFactura from "./InicioYRegistro/ModificarFactura.jsx";
 
 
 function App() {
@@ -24,13 +23,21 @@ function App() {
           <Route path="/InicioYRegistro" element={<InicioYRegistro />} />
           <Route path="/Terminosycondiciones" element={<Terminosycondiciones />} />
           <Route path="/carrito" element={<Carrito />} />
-          <Route path="/crud" element={<Crud />} />
-          <Route path="/solicitud" element={<Solicitud />} />
+          <Route path="/crud" element={<Privacidad ><Crud /></Privacidad>} />
+          <Route path="/devolucion" element={<Devolucion />} />
           <Route path="/factura" element={<Factura />} />
           <Route path="/vermas" element={<VerMas />} />
           <Route path="/RecoverPassword" element={<Recover />} />
           <Route path="/ResetPassword" element={<Reset />} />
-          <Route path="/devolucion" element={<Devolucion />} />
+          <Route path="/solicitud" element={<Solicitud />} />
+          <Route path="/modificar-factura/:numeroFactura" element={<ModificarFactura />} />
+
+          
+          
+
+
+          
+         
         </Routes>
       </div>
     </Router>
