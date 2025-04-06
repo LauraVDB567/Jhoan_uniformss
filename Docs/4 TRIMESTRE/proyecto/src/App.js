61256b5e6d13068239d4ff072101bfde3f8538b9
LuisaCastillo <163435574+LuisaCastillo109@users.noreply.github.com>
2025-04-06 17:41:22 -0500

    Update App.js

--- Docs/4 TRIMESTRE/proyecto/src/App.js
+++ Docs/4 TRIMESTRE/proyecto/src/App.js
@@ -2,16 +2,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Principal from './InicioYRegistro/Principal.jsx';
 import InicioYRegistro from './InicioYRegistro/InicioYRegistro.jsx';
 import Terminosycondiciones from './InicioYRegistro/Terminos y condiciones.jsx';
+import Recover from './InicioYRegistro/RecoverPassword.jsx';
+import Reset from './InicioYRegistro/ResetPassword.jsx';
 import Carrito from './InicioYRegistro/carrito.jsx';
 import Crud from "./InicioYRegistro/crud.jsx";
 import Devolucion from "./InicioYRegistro/devolucion.jsx";
 import Factura from './InicioYRegistro/factura.jsx'; 
 import VerMas from './InicioYRegistro/vermas.jsx';
-import Recover from './InicioYRegistro/RecoverPassword.jsx';
-import Reset from './InicioYRegistro/ResetPassword.jsx';
 import Solicitud from './InicioYRegistro/solicitud.jsx';
-import Privacidad from './InicioYRegistro/privacidad.js';
-import ModificarFactura from "./InicioYRegistro/ModificarFactura.jsx";
+import ModificarFactura from "./InicioYRegistro/ModificarFactura";
+import Privacidad from "./InicioYRegistro/privacidad.js"
 
 
 function App() {
@@ -23,21 +23,21 @@ function App() {
           <Route path="/InicioYRegistro" element={<InicioYRegistro />} />
           <Route path="/Terminosycondiciones" element={<Terminosycondiciones />} />
           <Route path="/carrito" element={<Carrito />} />
-          <Route path="/crud" element={<Privacidad ><Crud /></Privacidad>} />
-          <Route path="/devolucion" element={<Devolucion />} />
+          <Route
+            path="/crud"
+            element={
+              <Privacidad>
+                <Crud />
+              </Privacidad>
+            }
+          />
+          <Route path="/solicitud" element={<Solicitud />} />
           <Route path="/factura" element={<Factura />} />
           <Route path="/vermas" element={<VerMas />} />
           <Route path="/RecoverPassword" element={<Recover />} />
           <Route path="/ResetPassword" element={<Reset />} />
-          <Route path="/solicitud" element={<Solicitud />} />
+          <Route path="/devolucion" element={<Devolucion />} />
           <Route path="/modificar-factura/:numeroFactura" element={<ModificarFactura />} />
-
-          
-          
-
-
-          
-         
         </Routes>
       </div>
     </Router>
@@ -45,3 +45,4 @@ function App() {
 }
 
 export default App;
+
