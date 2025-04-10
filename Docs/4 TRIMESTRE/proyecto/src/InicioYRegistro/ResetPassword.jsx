333f4b4923febff02225a580831b1c2f3b2a9f33
LuisaCastillo <163435574+LuisaCastillo109@users.noreply.github.com>
2025-04-10 11:23:30 -0500

    Update ResetPassword.jsx

--- Docs/4 TRIMESTRE/proyecto/src/InicioYRegistro/ResetPassword.jsx
+++ Docs/4 TRIMESTRE/proyecto/src/InicioYRegistro/ResetPassword.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import "./syle/recuperar.css"
+
 
 const ResetPassword = () => { 
   const navigate = useNavigate();
