0fed65f09230f55b4a2244709d393873ef8799c2
LuisaCastillo <163435574+LuisaCastillo109@users.noreply.github.com>
2025-04-20 00:15:50 -0500

    Update imagenes.js

--- Docs/4 TRIMESTRE/proyecto/imagenes/imagenes.js
+++ Docs/4 TRIMESTRE/proyecto/imagenes/imagenes.js
@@ -1,30 +1,41 @@
 const express = require("express");
-const mysql = require ("mysql");
-const conn = require("express-myconnection")
-const routes = require ('./routes/image.routes')
-const app = express ();
+const mysql = require("mysql");
+const conn = require("express-myconnection");
+const imageRoutes = require('./routes/image.routes');
+const userRoutes = require('./routes/usuario.routes');
+const facturaRoutes = require('./routes/factura.routes'); 
+const app = express();
 const path = require("path");
-const carpetaCarrito = path.join(__dirname, '..', 'carrito'); 
+const devolucionRoutes = require('./routes/devolucion.routes');
+const RecuperarRoutes = require('./routes/recuperar.routes');
+const SolicitudRoutes = require ('./routes/solicitud.routes')
+
+const carpetaCarrito = path.join(__dirname, '..', 'carrito');
 app.use('/carrito', express.static(carpetaCarrito));
 
 const cors = require("cors");
 app.use(cors());
+app.use(express.json()); 
 
-app.set("port", process.env.PORT|| 5000)
-const dbConfig ={
-    host:"localhost",
-    user:"root",
-    password:"",
-    database :"datos"
+app.set("port", process.env.PORT || 5013);
+
+const dbConfig = {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "datos"
 };
 
-app.use (conn(mysql,dbConfig, 'single'));
+app.use(conn(mysql, dbConfig, 'single'));
 
+// Rutas
+app.use('/', imageRoutes);
+app.use('/', userRoutes); 
+app.use('/', facturaRoutes); 
+app.use('/', devolucionRoutes);
+app.use('/', RecuperarRoutes);
+app.use('/', SolicitudRoutes);
 
-app.use('/',routes)
-
-
-
-app.listen(app.get ('port'), ()=>{
-console.log("servidor funcionando por el puerto",app.get("port"));
+app.listen(app.get('port'), () => {
+    console.log("✅ Servidor funcionando por el puerto", app.get("port"));
 });
